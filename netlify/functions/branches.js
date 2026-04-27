8ab7427823b85931e0330023e5843dfb983c30cc
Ikigaiale <alejandraquijadab@gmail.com>
2026-04-27 13:17:09 -0400

    Update branches.js

--- netlify/functions/branches.js
+++ netlify/functions/branches.js
@@ -1,35 +1,36 @@
-const https = require('https');
-
-const SUCURSALES = [
-  { nombre: 'La Dehesa',   user: '7kqmjjngo',   pass: '41m7ggdejb0813u1g03w6itz27dr278jxzuu1tirk61umd9' },
-  { nombre: 'Las Condes',  user: 'h9ki8rg6',     pass: 'f2rpbfw0x3p13nflmdfmrif4fe3f0r3oo23lgxrgtm' },
-  { nombre: 'El Golf',     user: '3er0brdmo',    pass: 'gjdyvlew2iljhrsqg3z3x5afk1rpxu3gdxx1ws6wkh' },
-  { nombre: 'San Miguel',  user: '2ddclkg60',    pass: 'z4r41vrksjrsj53ueinu8i8ygcw4qmoz8ydwbrn700mizksx' },
-  { nombre: 'Antofagasta', user: '73dr15iec',    pass: '1vmwlx5jfl8pmfbu24i1aqne4sm1lbpc3i0ej13lbc998ov44k' },
-];
-
-exports.handler = async function(event, context) {
-  const results = await Promise.allSettled(
-    SUCURSALES.map(async s => {
-      const auth = Buffer.from(s.user + ':' + s.pass).toString('base64');
-      const data = await fetchJSON('agendapro.com', '/api/public/v1/locations', auth);
-      const list = Array.isArray(data) ? data : (data.locations || data.data || []);
-      const loc  = list[0] || {};
-      return {
-        nombre:   s.nombre,
-        address:  loc.address || '',
-        second_address: loc.second_address || '',
-        district: loc.district || '',
-        city:     loc.city || '',
-        phone:    loc.phone || '',
-        booking_url: 'https://sculptbody.agendapro.com/cl'
-      };
-    })
-  );
-
-  const branches = results
-    .filter(r => r.status === 'fulfilled')
-    .map(r => r.value);
+exports.handler = async function() {
+  const branches = [
+    {
+      nombre: 'La Dehesa',
+      address: 'La Dehesa', city: 'Santiago',
+      phone: '',
+      booking_url: 'https://sculptbodyladehesa.site.agendapro.com/cl/sucursal/392853'
+    },
+    {
+      nombre: 'Las Condes',
+      address: 'Avenida las Condes 9460', city: 'Santiago',
+      phone: '+56 941 444 094',
+      booking_url: 'https://sculptbody.site.agendapro.com/cl/sucursal/25115'
+    },
+    {
+      nombre: 'El Golf',
+      address: 'El Golf', city: 'Santiago',
+      phone: '',
+      booking_url: 'https://sculptbodyelgolf.site.agendapro.com/cl/sucursal/170206'
+    },
+    {
+      nombre: 'San Miguel',
+      address: 'San Miguel', city: 'Santiago',
+      phone: '',
+      booking_url: 'https://sculptbodysanmiguel.site.agendapro.com/cl/sucursal/115068'
+    },
+    {
+      nombre: 'Antofagasta',
+      address: 'Antofagasta', city: 'Antofagasta',
+      phone: '',
+      booking_url: 'https://sculptbodyantofagasta.site.agendapro.com/cl/sucursal/361946'
+    },
+  ];
 
   return {
     statusCode: 200,
@@ -37,22 +38,3 @@ exports.handler = async function(event, context) {
     body: JSON.stringify({ branches })
   };
 };
-
-function fetchJSON(host, path, auth) {
-  return new Promise((resolve, reject) => {
-    const req = https.request(
-      { hostname: host, path, method: 'GET',
-        headers: { 'Authorization': 'Basic ' + auth, 'Accept': 'application/json' } },
-      res => {
-        let body = '';
-        res.on('data', c => body += c);
-        res.on('end', () => {
-          if (res.statusCode >= 400) reject(new Error('HTTP ' + res.statusCode));
-          else { try { resolve(JSON.parse(body)); } catch(e) { reject(new Error('JSON')); } }
-        });
-      }
-    );
-    req.on('error', reject);
-    req.end();
-  });
-}
